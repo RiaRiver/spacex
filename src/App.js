6c2fd0ca232728f1e28b47ded4039980ca7bb122
RiaRiver <riria.river@gmail.com>
2020-10-16 04:56:51 +0300

    lesson: day 2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,54 @@ import React from 'react';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Features from "./components/Features/Features";
-import Calendar from "./components/Calendar/Calendar";
 import Footer from "./components/Footer/Footer";
 
-import './style.css';
-import Details from "./components/Details/Details";
+import FetchData from "./service/FetchData";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <Main />
-      <Features />
-      <Calendar />
-      <Details />
-      <Footer />
-    </React.Fragment>
-  );
+import './style.css';
+
+class App extends React.Component {
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+  };
+
+  componentDidMount() {
+    this.updateRocket();
+
+  }
+
+
+  updateRocket() {
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({rockets: data.map(item => item.name)});
+        return data;
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({rocketFeatures}));
+  }
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket)
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket}/>
+        <Main rocket={this.state.rocket}/>
+        <Features/>
+        <Footer/>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/service/FetchData.js
@@ -0,0 +1,20 @@
+export default class FetchData {
+  startUrl = 'https://api.spacexdata.com/v4/';
+
+  getData = async url => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Произошла ошибка ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getRocket = async () => await this.getData(this.startUrl + 'rockets');
+
+  getLaunches = async () => await this.getData(this.startUrl + 'launches/past');
+
+  getCompany = async () => await this.getData(this.startUrl + 'company');
+}
+
